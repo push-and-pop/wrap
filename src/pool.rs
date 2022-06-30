df42afec0d87fe2cdc056e9b5da2d7e81211cfd8
zhaha <80676432+push-and-pop@users.noreply.github.com>
2022-06-30 18:29:51 +0800

    aa

--- src/pool.rs
+++ src/pool.rs
@@ -1,15 +1,43 @@
-use crossbeam::atomic::AtomicCell;
+use crate::stack::Stack;
+use std::ptr;
 pub trait Recycle: Default {
-    fn get() {}
-
-    fn reset() {}
+    fn reset(&mut self) {}
 }
 
-pub struct Pool<T> {
-    value: Vec<AtomicCell<T>>,
+#[derive(Debug)]
+pub struct ObjectPool<T> {
+    value: *mut Stack<T>,
 }
+
+impl<T: Recycle> ObjectPool<T> {
+    pub fn new() -> Self {
+        let ref mut stack = Stack::new();
+        Self {
+            value: stack as *mut Stack<T>,
+        }
+    }
+
+    pub fn get(&self) -> T {
+        unsafe {
+            match (*self.value).pop() {
+                Some(t) => t,
+                None => T::default(),
+            }
+        }
+    }
+
+    pub fn put(&self, mut t: T) {
+        t.reset();
+        unsafe {
+            (*self.value).push(t);
+        }
+    }
+}
+impl<T: Default> Recycle for Box<T> {}
 
 mod test {
     #[test]
-    fn test() {}
+    fn test() {
+        let a: i32 = i32::default();
+    }
 }
